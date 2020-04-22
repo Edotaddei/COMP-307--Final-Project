7e504332a59da78fd94502537b7b367cc9a11b3a
inesmoreno <39106109+inesmoreno@users.noreply.github.com>
2020-04-22 14:45:15 -0400

    Final

--- marketplaceNEW/listing/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(OrderProduct)
-admin.site.register(Order)
-admin.site.register(Address)
-#admin.site.register(Ce)
